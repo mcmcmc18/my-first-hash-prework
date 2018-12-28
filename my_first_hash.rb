4c6e3b279bca0eba2036a9707956ef80d8cca227
MC Simoes <mariacristina.simoes@gmail.com>
2018-12-28 20:48:44 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -6,7 +6,7 @@ end
 def shipping_manifest
 the_manifest = { "whale bone corsets" => 5,
                  "porcelain vases" => 2,
-                 "oil paintings" => 3
+                 "oil paintings" => 3}
 end
 
 def retrieval
@@ -20,11 +20,10 @@ def adding
   shipping_manifest = {
                   "whale bone corsets" => 5, 
                   "porcelain vases" => 2, 
-                  "oil paintings" => 3
-                 }
+                  "oil paintings" => 3}
 
   shipping_manifest["muskets"] = 2 
   shipping_manfiest["gun powder"] = 4
 shipping_manifest
 end
-
+end
